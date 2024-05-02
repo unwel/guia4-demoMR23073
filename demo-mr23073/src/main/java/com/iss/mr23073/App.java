3a5b0b57ffcb8667b28ee684839a8d91a6045b51
unwel <unwellos16@gmail.com>
2024-05-02 11:58:55 -0600

    Primer commit

---
+++ demo-mr23073/src/main/java/com/iss/mr23073/App.java
@@ -0,0 +1,17 @@
+package com.iss.mr23073;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "hello World";
+	}
+    public static void main( String[] args )
+    {
+    	App hw = new App();
+        System.out.println(hw.Hello() );
+    }
+}
---
+++ demo-mr23073/src/test/java/com/iss/mr23073/AppTest.java
@@ -0,0 +1,20 @@
+package com.iss.mr23073;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testHello()
+    {
+        assertTrue( true );
+    }
+}
